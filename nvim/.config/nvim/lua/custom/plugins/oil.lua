a7208c0aec336b39f3134a6fbe69fbb3506c8889
Prayrex <scharfe.edwin@gmail.com>
2025-05-07 12:24:55 +0200

    show hidden files (not working, but somehow makes nvim transparent in tmux)

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -1,11 +1,22 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  keys = {
-    { '-', '<cmd>Oil<CR>', desc = 'Open parent directory with oil' },
-  },
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	keys = {
+		{ "-", "<cmd>Oil<CR>", desc = "Open parent directory with oil" },
+	},
+	view_options = {
+		show_hidden = true,
+		is_hidden_file = function(name, bufnr)
+			local m = name:match("^%.")
+			return m ~= nil
+		end,
+		-- This function defines what will never be shown, even when `show_hidden` is set
+		is_always_hidden = function(name, bufnr)
+			return false
+		end,
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
 }
